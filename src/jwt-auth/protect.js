af65bdd2b3f805fb3801db1526b3bef789160741
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-02-25 22:39:43 +0530

    feat:add authorizeUser middleware

--- src/jwt-auth/protect.js
+++ src/jwt-auth/protect.js
@@ -1,4 +1,5 @@
-import jwt from "jsonwebtoken";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+import User from "../models/User";
 
 
 export const generateToken = (userId)=>{
@@ -8,3 +9,21 @@ export const generateToken = (userId)=>{
         {algorithm: 'HS256' , expiresIn:'7d'}
     ))
 };
+
+export const authorizeUser = async(req,res,next)=>{
+    try{
+    const authHeader = req.headers.authorization;
+    if(!authHeader||!authHeader.startsWith('Bearer ')) return res.status(401).json({message:"User not authorized"});
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    const userId = decoded.userId;
+    const user = await User.findById(userId).select('_id').lean();
+    if(!user) return res.status(401).json({message:"Your account is deleted"});
+    req.userId = userId;
+    next()
+    }catch(error){
+        if(error.name ==="JsonWebTokenError") return res.status(401).json({message:"Invalid token"})
+        if(error.name === "TokenExpiredError") return res.status(401).json({message:"Need to login again"})
+        return res.status(500).json({message:"internal server error"});    
+    };
+}
