65d23d21b0197b2b791f71eac521e87a23848874
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-02-27 20:58:19 +0530

    feat:setup jest testing environment with mongodb-memory-server

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default{
+     testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/__tests__/setup.js"],
+  testMatch: ["**/__tests__/**/*.test.js"],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/**/*.test.js",
+    "!**/node_modules/**",
+  ],
+  coveragePathIgnorePatterns: ["/node_modules/", "/__tests__/"],
+};
