f0aceb983b1bc65ad37d9c2e2fa9166c6fa62a81
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-02-27 14:59:45 +0530

    fix: correct user model import and remove unused jwt

--- src/jwt-auth/protect.js
+++ src/jwt-auth/protect.js
@@ -1,5 +1,5 @@
-import jwt, { JsonWebTokenError } from "jsonwebtoken";
-import User from "../models/User";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 
 export const generateToken = (userId)=>{
